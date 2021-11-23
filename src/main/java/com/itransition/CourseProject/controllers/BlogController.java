1a3d01be21e066dd8ba6508adbefd4dee8be3f1a
kolesyonkova <katerina.kolesyonkova.01@mail.ru>
2021-11-23 18:57:22 +0300

    Initial commit

---
+++ src/main/java/com/itransition/CourseProject/controllers/BlogController.java
@@ -0,0 +1,13 @@
+package com.itransition.CourseProject.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class BlogController {
+    @GetMapping("/blog")
+    public String blogMain(Model model) {
+        return "blog-main";
+    }
+}
